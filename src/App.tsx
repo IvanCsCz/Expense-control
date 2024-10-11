f17ab32bd4ca7f6088d38cb30839cbed0192cd09
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-10-10 20:48:46 -0600

    Change text to Spanish

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import { useMemo } from "react"
 import BudgetForm from "./components/BudgetForm"
+import BudgetTracker from "./components/BudgetTracker"
+import { useBudget } from "./hooks/useBudget"
 
 function App() {
+  const {state} = useBudget()
+
+  const isValidBudget = useMemo(() => state.budget > 0 , [state.budget])
+
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
         <h1 className="text-center uppercase font-black text-4xl text-white">
-          Expense control
+          Planificador de gastos
         </h1>
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm />
+        {isValidBudget ? <BudgetTracker /> : <BudgetForm />}        
       </div>
     </>
   )
--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,10 +1,17 @@
-import { ChangeEvent, useMemo, useState } from "react"
+import { ChangeEvent, FormEvent, useMemo, useState } from "react"
+import { useBudget } from "../hooks/useBudget"
 
 function BudgetForm() {
   const [budget, setBudget] = useState(0)
+  const {dispatch} = useBudget()
 
   const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
     setBudget(ev.target.valueAsNumber)
+  }
+
+  const handleSubmit = (ev: FormEvent<HTMLFormElement>) => {
+    ev.preventDefault()
+    dispatch({type: 'add-budget', payload: {budget}})
   }
 
   const isValid = useMemo(() => {
@@ -12,9 +19,9 @@ function BudgetForm() {
   }, [budget])
 
   return (
-    <form className="space-y-5">
+    <form onSubmit={handleSubmit} className="space-y-5">
       <div className="flex flex-col space-y-5">
-        <label htmlFor="budget" className="text-4xl font-bold text-blue-600 text-center">Define budget</label>
+        <label htmlFor="budget" className="text-4xl font-bold text-blue-600 text-center">Definir Presupuesto</label>
         <input 
           type="number" 
           id="budget" 
@@ -28,7 +35,7 @@ function BudgetForm() {
 
       <input 
         type="submit" 
-        value="Define budget"
+        value="Definir Presupuesto"
         className="w-full cursor-pointer p-2 font-bold bg-blue-600 hover:bg-blue-700 text-white uppercase disabled:opacity-30"
         disabled={isValid}
       />
